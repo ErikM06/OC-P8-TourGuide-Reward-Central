08812201e6ced20f09c48ac6b44227322b61c64c
ErikM06 <murer.erik@gmail.com>
2022-05-06 09:57:45 +0200

    first commit

---
+++ src/main/java/com/rewardcentral/RewardCentralMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rewardcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardCentralMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardCentralMicroServiceApplication.class, args);
+    }
+
+}
