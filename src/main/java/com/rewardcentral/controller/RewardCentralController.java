b7b9faffe414046986d3a302052554b4894b6c85
ErikM06 <murer.erik@gmail.com>
2022-05-06 10:20:49 +0200

    service, controller and Exception for RewardCentral

---
+++ src/main/java/com/rewardcentral/controller/RewardCentralController.java
@@ -0,0 +1,38 @@
+package com.rewardcentral.controller;
+
+import com.rewardcentral.exceptions.UUIDException;
+import com.rewardcentral.service.RewardCentralService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class RewardCentralController {
+
+    @Autowired
+    RewardCentralService rewardCentralService;
+
+    /**
+     * 
+     * @param attractionId get attraction UUID as a string
+     * @param userId get user UUID as a string
+     * @return int the rewardsPoints
+     * @throws UUIDException if something went wrong parsing String id to UUID
+     */
+    public int getRewardPointFromService (@RequestParam String attractionId, String userId) throws UUIDException {
+        UUID attractionUUID = null;
+        UUID userUUID = null;
+        try {
+            attractionUUID= UUID.fromString(attractionId);
+            userUUID=UUID.fromString(userId);
+
+        } catch (IllegalArgumentException e) {
+            throw new UUIDException(attractionId,userId);
+
+        }
+        return rewardCentralService.getRewardPointsRewardsCentral(attractionUUID, userUUID);
+    }
+
+}
